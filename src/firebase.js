a9157123205b063482a25babca92a58e8bcbf444
Sadekur <shadekur.rahman60@gmail.com>
2026-08-24 21:49:25 +0600

    refactor: remove commented-out Firebase configuration lines in firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  //https://console.firebase.google.com/u/4/project/store-managment-system-d0019/overview
   apiKey: "AIzaSyCwKzDLzmmeAkfh2N4BgX6m11eDIGYKe0I",
   authDomain: "store-managment-system-d0019.firebaseapp.com",
   projectId: "store-managment-system-d0019",
   storageBucket: "store-managment-system-d0019.firebasestorage.app",
   messagingSenderId: "304010652482",
   appId: "1:304010652482:web:c78cc27fa1de88f164b6db",
-  // measurementId: "G-9M0S35D9MV"
 };
 
 const app = initializeApp(firebaseConfig);
